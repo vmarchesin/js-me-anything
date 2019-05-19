a6e1a64df0d474c827a89bb57a6f69aab6a8d369
Vinicius Araujo <vinicius.marchesinaraujo@gmail.com>
2019-05-19 21:37:06 +0200

    Layout changes
    Removed the css file and added it to the global style using styled-components.
    
    Changed the card box-shadow to be more subtle

--- src/components/Card/style/index.js
+++ src/components/Card/style/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledCard = styled.div`
   background-color: #f3f3f3;
-  border: 1px solid #bebebe;
+  border: 1px solid #d0d0d0;
 
   border-radius: 4px;
   cursor: pointer;
@@ -16,7 +16,7 @@ const StyledCard = styled.div`
   transition: 0.1s ease-in;
   &:hover {
     transform: scale(1.02);
-    box-shadow: 0px 5px 10px -4px rgba(0,0,0,0.75);
+    box-shadow: 0px 5px 10px -6px rgba(0,0,0,0.25);
   }
 `;
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,15 +7,57 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { StaticQuery, graphql } from 'gatsby';
 import { ApolloProvider } from 'react-apollo';
 
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import './layout.css';
 
 import client from '@apollo/client';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  html {
+    font: 112.5%/1.45em 'Roboto', georgia, serif;
+    box-sizing: border-box;
+    overflow-y: scroll;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+  }
+  body {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  a {
+    background-color: transparent;
+    outline: none;
+    color: inherit;
+    text-decoration: none;
+    -webkit-text-decoration-skip: objects;
+  }
+  button {
+    font: inherit;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+  p {
+    margin: 0;
+    padding: 0;
+    margin-bottom: 1.45rem;
+  }
+  pre {
+    border-radius: 8px;
+  }
+  @media only screen and (max-width: 480px) {
+    html {
+      font-size: 100%;
+    }
+  }
+`;
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -50,6 +92,7 @@ const Layout = ({ children }) => (
     render={data => (
       <Wrapper>
         <Header siteTitle={data.site.siteMetadata.title} />
+        <GlobalStyle />
         <div
           style={{
             margin: `0 auto`,
