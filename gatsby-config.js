7683458f147a1e524e02ba50da0e1defdfe08a8a
Vinicius Araujo <vinicius.marchesinaraujo@gmail.com>
2019-06-03 18:46:48 +0200

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,50 @@
 module.exports = {
   pathPrefix: '/js-me-anything',
   siteMetadata: {
-    title: `JS Me Anything`,
-    description: `A place to test your JavaScript skills. Prepare yourself for the next interview.`,
-    author: `@devmarchesin`,
+    title: 'JS Me Anything',
+    description:
+      'A place to test your JavaScript skills. Prepare yourself for the next interview.',
+    author: '@devmarchesin',
   },
   plugins: [
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#f9ce00`,
-        theme_color: `#f9ce00`,
-        display: `minimal-ui`,
-        icon: `src/images/js-logo.png`,
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#f9ce00',
+        theme_color: '#f9ce00',
+        display: 'minimal-ui',
+        icon: 'src/images/js-logo.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: [
+          'UA-141323871-1', // Google Analytics / GA
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+          send_page_view: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: true,
+          respectDNT: true,
+        },
       },
     },
     {
